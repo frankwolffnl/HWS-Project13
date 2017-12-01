7f06fc24b9ce3070a320665760e71fdb94d8d329
frankwolffnl <frankwolff.nl@gmail.com>
2017-12-01 11:44:36 +0100

    Finished project 13 of Hacking With Swift.

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -63,7 +63,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 		present(ac, animated: true)
 	}
 	
-	@IBAction func save(_ sender: UIButton) {
+	@IBAction func save(_ sender: Any) {
+		UIImageWriteToSavedPhotosAlbum(imageView.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+	}
+	
+	@objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+		if let error = error {
+			// We got back an error!
+			let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+			ac.addAction(UIAlertAction(title: "OK", style: .default))
+			present(ac, animated: true)
+		} else {
+			let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photo's.", preferredStyle: .alert)
+			ac.addAction(UIAlertAction(title: "OK", style: .default))
+			present(ac, animated: true)
+		}
 	}
 	
 	@IBAction func intensityChanged(_ sender: Any) {
