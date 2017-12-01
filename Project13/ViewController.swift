4c6c9e96bebebb5ce4560d80ff6f4fc53ab4c70e
frankwolffnl <frankwolff.nl@gmail.com>
2017-12-01 11:03:23 +0100

    Basic UI done.

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var intensity: UISlider!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	@IBAction func changeFilter(_ sender: UIButton) {
+	}
+	
+	@IBAction func save(_ sender: UIButton) {
+	}
+	
+	@IBAction func intensityChanged(_ sender: UISlider) {
+	}
+	
 }
 
